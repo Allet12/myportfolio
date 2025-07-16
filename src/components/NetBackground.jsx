1c164b23e5ce92dd8ba09385096c92bcbfde614f
Javainterlproject <mngomezuluallet@gmail.com>
2025-07-16 03:27:50 +0200

    feat: update theme defaults and colors

--- src/components/NetBackground.jsx
+++ src/components/NetBackground.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 
-export const StarBackground = () => {
+export const NetBackground = () => {
   const canvasRef = useRef(null);
   const animationRef = useRef(null);
 
